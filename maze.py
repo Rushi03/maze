51ebdba7c23cf3a32fe2c4cd317e632c5413706f
Rushi Patel <rushi.m.patel92@gmail.com>
2017-04-28 21:21:33 -0700

    minor fixes
    fix: Minor fixes
    
    Made fixes to 'move' method making the change in location set in bound the maze dimensions.
    
    No footer

--- maze.py
+++ maze.py
@@ -39,23 +39,23 @@ class Maze(object):
         # vertical walls
         for x in range(self.dim-1):
             for y in range(self.dim):
-                if (self.walls[x,y] & 2 != 0) != (self.walls[x+1,y] & 8 != 0):
-                    wall_errors.append([(x,y), 'v'])
+                if (self.walls[x, y] & 2 != 0) != (self.walls[x+1,y] & 8 != 0):
+                    wall_errors.append([(x, y), 'v'])
         # horizontal walls
         for y in range(self.dim-1):
             for x in range(self.dim):
-                if (self.walls[x,y] & 1 != 0) != (self.walls[x,y+1] & 4 != 0):
+                if (self.walls[x, y] & 1 != 0) != (self.walls[x,y+1] & 4 != 0):
                     wall_errors.append([(x,y), 'h'])
 
         if wall_errors:
             for cell, wall_type in wall_errors:
                 if wall_type == 'v':
                     cell2 = (cell[0]+1, cell[1])
-                    print 'Inconsistent vertical wall betweeen {} and {}'.format(cell, cell2)
+                    print "Inconsistent vertical wall betweeen {} and {}".format(cell, cell2)
                 else:
                     cell2 = (cell[0], cell[1]+1)
-                    print 'Inconsistent horizontal wall betweeen {} and {}'.format(cell, cell2)
-            raise Exception('Consistency errors found in wall specifications!')
+                    print "Inconsistent horizontal wall betweeen {} and {}".format(cell, cell2)
+            raise Exception("Consistency errors found in wall specifications!")
 
     def is_permissible(self, cell, direction):
         """
@@ -69,7 +69,7 @@ class Maze(object):
         try:
             return (self.walls[tuple(cell)] & dir_int[direction] != 0)
         except:
-            print 'Invalid direction provided!'
+            print "Invalid direction provided!"
 
     def dist_to_wall(self, cell, direction):
         """
@@ -83,7 +83,7 @@ class Maze(object):
 
         sensing = True
         distance = 0
-        curr_cell = list(cell)  # make copy to preserve original
+        curr_cell = list(cell)  # Make copy to preserve original
         while sensing:
             if self.is_permissible(curr_cell, direction):
                 distance += 1
@@ -93,42 +93,77 @@ class Maze(object):
                 sensing = False
         return distance
 
-    def move(self, location, action):
-        goal = (self.dim / 2 - 1, self.dim / 2)
+    def move(self, heading, location, action, rotate, movement):
+        # Goal set for robot to reach
+        self.goal = (self.dim / 2 - 1, self.dim / 2)
+
         # Direction moves for the robot
-        delta = [[0, 1],   # Move up
-                 [1, 0],   # Move right
-                 [0, -1],  # Move down
-                 [-1, 0]]  # Move left
+        delta = {'up': [0, -1], 'right': [1, 0], 'down': [0, 1], 'left': [-1, 0]}
+        # Robot direction sensing
+        sensor_direction = {'up': ['left', 'up', 'right'], 'right': ['up', 'right', 'left'],
+                            'down': ['right', 'down', 'left'], 'left': ['down', 'left', 'up']}
+        # Going down
+        reverse = {'up': 'down', 'right': 'left', 'down': 'up', 'left': 'right'}
 
-        # Reward starts between -1 and 1; [-1, 1]
-        reward = 2 * random.random() - 1
+        # Sense distance to the wall
+        sensing = [self.dist_to_wall(location, heading)
+                   for heading in sensor_direction[heading]]
 
-        if self.is_permissible(location, action):
-            if action == 'forward':
-                location[0] = (location[0] - delta[0][0]) % self.dim
-                location[1] = (location[1] - delta[0][1]) % self.dim
-                reward += -0.25
-            elif action == 'right':
-                location[0] = (location[0] + delta[1][0]) % self.dim
-                location[1] = (location[1] + delta[1][1]) % self.dim
-                reward += -0.5
-            elif action == 'backwards':
-                location[0] = (location[0] - delta[2][0]) % self.dim
-                location[1] = (location[1] - delta[2][1]) % self.dim
-                reward += -1.5
-            elif action == 'left':
-                location[0] = (location[0] + delta[3][0]) % self.dim
-                location[1] = (location[1] + delta[3][1]) % self.dim
-                reward += -1.25
-            else:
-                reward += -2
+        if rotate == -90:
+            heading = sensor_direction[heading][0]
+        elif rotate == 90:
+            heading = sensor_direction[heading][2]
+        elif rotate == 0:
+            pass
         else:
-            location = location
-    
-        # Reward for reaching the goal
-        if location[0] in goal and location[1] in goal:
-            reward += 10
+            print "Invalid rotation value."
+
+        # self.bounds = [1, 2, self.dim, self.dim + 1]
+        # location[0] = (location[0] + delta[heading][0] - self.bounds[0]) % (self.bounds[2] - self.bounds[0] + 1) + self.bounds[0]
+        # location[1] = (location[1] + delta[heading][1] - self.bounds[1]) % (self.bounds[3] - self.bounds[1] + 1) + self.bounds[1]
+        # location[0] = (location[0] + delta[reverse][0] - self.bounds[0]) % (self.bounds[2] - self.bounds[0] + 1) + self.bounds[0]
+        # location[1] = (location[1] + delta[reverse][1] - self.bounds[1]) % (self.bounds[3] - self.bounds[1] + 1) + self.bounds[1]
+
+        if abs(movement) > 3:
+            print "Only can move 3 squares per turn."
+        movement = max(min(int(movement), 3), -3)  # Range [-3, 3]
+        while movement:
+            if movement > 0:
+                if self.is_permissible(location, heading):
+                    location[0] = (location[0] + delta[heading][0]) % self.dim
+                    location[1] = (location[1] + delta[heading][1]) % self.dim
+                    movement -= 1
+                else:
+                    "Ran into wall. Movement stopped."
+                    movement = 0
+            else:
+                reverse = reverse[heading]
+                if self.is_permissible(location, heading):
+                    location[0] = (location[0] + delta[reverse][0]) % self.dim
+                    location[1] = (location[1] + delta[reverse][1]) % self.dim
+                    movement += 1
+                else:
+                    "Ran into wall. Movement stopped."
+                    movement = 0
+
+        # self.reward starts between -1 and 1; [-1, 1]
+        self.reward = 2 * random.random() - 1
+
+        if action == 'up':
+            self.reward += -0.25
+        elif action == 'right':
+            self.reward += -0.5
+        elif action == 'down':
+            self.reward += -1.5
+        elif action == 'left':
+            self.reward += -1.25
+        else:
+            self.reward += -2
+
+        print location
+        # self.reward for reaching the goal
+        if location[0] in self.goal and location[1] in self.goal:
+            self.reward += 10
             location[0] = 0
-            location[1] = 11
-        return reward
+            location[1] = self.dim - 1
+        return self.reward
